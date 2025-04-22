39ff1183d3cef072609d43ab16cad6b45f59f952
amitk9627 <ramitgzp56@gmail.com>
2025-04-22 23:47:30 +0530

    backendURL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BackendUrl = 'https://clikdbackend.up.railway.app/';
+export const BackendUrl = 'https://clikdbackend.up.railway.app';
 // export const BackendUrl = 'http://localhost:3001';
 
 
